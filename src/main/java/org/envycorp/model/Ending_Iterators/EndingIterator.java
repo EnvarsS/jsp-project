e0f0e59a8f3d1cf4407beec5cb604ac4d1aca04f
Vladyslav <eddiedipper4228@gmail.com>
2025-09-27 10:57:44 +0200

    Enhance EndingIterator with logging and improve scene retrieval methods

--- src/main/java/org/envycorp/model/Ending_Iterators/EndingIterator.java
+++ src/main/java/org/envycorp/model/Ending_Iterators/EndingIterator.java
@@ -2,12 +2,19 @@ package org.envycorp.model.Ending_Iterators;
 
 import org.envycorp.model.Scene_Elements.Scene;
 import org.envycorp.util.SceneLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
 public abstract class EndingIterator {
-    private ArrayList<Scene> scenes;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EndingIterator.class);
+
+    private final ArrayList<Scene> scenes;
     private final SceneLoader sceneLoader;
+
+    private static final int GOOD_ENDING_INDEX = 0;
+    private static final int BAD_ENDING_INDEX = 1;
 
     public EndingIterator(String filePath){
         this.sceneLoader = new SceneLoader();
@@ -15,11 +22,13 @@ public abstract class EndingIterator {
     }
 
     public Scene getGoodEnding(){
-        return scenes.getFirst();
+        LOGGER.info("Getting good ending iterator");
+        return scenes.get(GOOD_ENDING_INDEX);
     }
 
     public Scene getBadEnding(){
-        return scenes.get(1);
+        LOGGER.info("Getting bad ending iterator");
+        return scenes.get(BAD_ENDING_INDEX);
     }
 
 
